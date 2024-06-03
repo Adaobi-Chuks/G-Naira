370aba203d2e2dc2ef7114018e46ea112940aa16
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-06-03 21:54:48 +0100

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -32,7 +32,7 @@ const findMetaMaskAccount = async () => {
 };
 
 function App() {
-  const contractAddress = "0x4801C3742A3E5dc32B1E1419fC40ED765d447AFf";
+  const contractAddress = "0x417100B9D42CdB746fbeF46Eb0A7a651743B155A";
   const contractABI = abi.abi;
   const [currentAccount, setCurrentAccount] = useState("");
   const [balance, setBalance] = useState(0);
